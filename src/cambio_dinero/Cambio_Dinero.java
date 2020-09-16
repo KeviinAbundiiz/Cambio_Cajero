19ed498898833473cce6fb324c949f50f31ed7c4
KeviinAbundiiz <abundiiz_keviin@outlook.es>
2020-09-16 00:12:12 -0500

    ProgramaRealizado

---
+++ src/cambio_dinero/Cambio_Dinero.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cambio_dinero;
+
+/**
+ *
+ * @author Abund
+ */
+public class Cambio_Dinero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cambio  cam = new Cambio();
+        
+        cam.setTitle("Problema del cambio de dinero");
+        cam.setLocationRelativeTo(null);
+        cam.setVisible(true);
+    }
+    
+}
